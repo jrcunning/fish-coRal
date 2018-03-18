69f7f90915717f3609a3280ccadf10e7638b44e6
Ross Cunning <ross.cunning@gmail.com>
2018-03-18 11:43:44 -0400

    initial code base

---
+++ R/def_pars.R
@@ -0,0 +1,65 @@
+#' Get default model parameters.
+#'
+#' \code{def_pars} takes no arguments and returns the default values for all
+#' parameters used in the model.
+#'
+#' @return A list of 26 length-one numerics containing the default values for
+#'   all parameters used in the model. Each list element is named as the
+#'   parameter name.
+#' @examples
+#' def_pars()
+#' pars <- def_pars()
+
+def_pars <- function(nsym=1) {
+  return(list(
+    jHT0=0.03,  # Host specific biomass turnover rate (d^-1)
+    nNH=0.18,  # N:C ratio in host biomass (-)
+    nNX=0.2,  # N:C ratio in prey biomass (-)
+    sigmaNH=0.9,  # Proportion of host nitrogen turnover recycled (-)
+    sigmaCH=0.1,  # Proportion of host carbon turnover recycled (-)
+    jXm=0.13,  # Maximum specific host feeding rate (molX/CmolH/d)
+    jNm=0.035,  # Maximum specific host DIN uptake rate (molN/CmolH/d)
+    jHGm=1,  # Maximum specific host growth rate (CmolH/CmolH/d)
+    kCO2=10,  # Rate of host CCM's (molCO2/molC/d)
+    KN=1.5e-6,  # Half-saturation constant for host DIN uptake (molN/L)
+    KX=1e-6,  # Half-saturation constant for host feeding (CmolX/L)
+    initH=1,  # Initial host biomass (CmolH)
+    yC=0.8,
+    jST0=rep(0.03, nsym),  # Symbiont specific biomass turnover rate (d^-1)
+    nNS=rep(0.13, nsym),  # N:C ratio in symbiont biomass (-)
+    yCL=rep(0.1, nsym),  # L:C ratio in fixed carbon (=quantum yield) (molC/mol ph)
+    kNPQ=rep(112, nsym),  # capacity of non-photochemical quenching (mol ph/CmolS/d)
+    # calculated as 4x max. photochemical quenching (Gorbunov et al. 2001)
+    kROS=rep(80, nsym),  # amount of excess light beyond NPQ capacity (e.g., jeL-jNPQ) that doubles ROS production relative to baseline (mol ph/CmolS/d)
+    k=rep(1, nsym),  # exponent on ROS production (-)
+    astar=rep(1.34, nsym),  # Symbiont specific cross-sectional area (m^2/C-molS)
+    sigmaNS=rep(0.9, nsym),  # Proportion of symbiont nitrogen turnover recylced (-)
+    sigmaCS=rep(0.9, nsym),  # Proportion of symbiont carbon turnover recycled (-)
+    jCPm=rep(2.8, nsym),  # Maximum specific photosynthate production rate (Cmol/CmolS/d)
+    jSGm=rep(0.25, nsym),  # Maximum specific symbiont growth rate (CmolS/CmolS/d)
+    initS=rep(1, nsym),  # Initial symbiont biomass (CmolS)
+    b=rep(5, nsym),  # Scaling parameter for bleaching response
+    kv=1,  #cubic meters per C-mol H
+    gamma=1,
+    m=1, # 1/(d * (cubic meters)^mu)
+    mu=1,
+    # Damselfish parameters (P)
+    rp=1, # intrinsic growth rate
+    kp=1,
+    Bp=1,
+    alpha.wp=1,
+    ap=1,  # attack rate on damselfish
+    # Hawkfish parameters (W)
+    rw=1, # intrinsic growth rate
+    kw=1,
+    Bw=1,
+    alpha.pw=1,
+    aw=1,  # attack rate on hawkfish
+    U=1,  # Uber-predator abundance
+    ep=1,  # damselfish N excretion rate (N per biomass)
+    ew=1,  # hawkfish N excretion rate (N per biomass)
+    D=1,  # flushing rate of N between interstitial volume and ambient environment
+    vi=0.5  # fraction of coral volume occupied by water
+  ))
+}
+
---
+++ R/run_fish_coral.R
@@ -0,0 +1,141 @@
+
+run_fish_coral <- function(time, env, pars) {
+  
+  nsym <- length(pars$initS)  # Get number of symbionts in run
+  vsynth <- Vectorize(synth)  # Vectorize synthesizing unit function for nsym > 1
+
+  # Set initial values
+  # ==================
+  # Host fluxes
+  ## Create empty vectors for each flux with length equal to time vector
+  for(x in c("jX", "jN", "rNH", "rhoN", "jeC", "jCO2", "jHG", "jHT", "rCH", "dH.Hdt", "H")) {
+    assign(x, rep(NA, length(time)))
+  }
+  ## Set initial values for host fluxes
+  jX[1] <- (pars$jXm * env$X[1] / (env$X[1] + pars$KX))
+  jN[1] <- (pars$jNm * env$N[1] / (env$N[1] + pars$KN))
+  rNH[1] <- pars$jHT0 * pars$nNH * pars$sigmaNH
+  rhoN[1] <- jN[1]
+  jeC[1] <- 10
+  jCO2[1] <- pars$kCO2 * jeC[1]
+  jHG[1] <- 0.25
+  jHT[1] <- pars$jHT0
+  rCH[1] <- pars$jHT0 * pars$sigmaCH
+  dH.Hdt[1] <- pars$jHGm
+  H[1] <- pars$initH
+  # Symbiont fluxes
+  ## Create empty vectors for each flux with length equal to time vector
+  for (x in c("rNS", "jL", "jCP", "jeL", "jNPQ", "jCO2w", "jSG", "rhoC", "jNw", "jST", "rCS", "cROS", "dS.Sdt", "S")) {
+    assign(x, matrix(NA, ncol=nsym, nrow=length(time)))
+  }
+  ## Set initial values for symbiont fluxes
+  rNS[1,] <- pars$jST0 * pars$nNS * pars$sigmaNS
+  jL[1,] <- env$L[1] * pars$astar
+  jCP[1,] <- pmax(0, vsynth(jL[1,] * pars$yCL, jCO2[1]*H[1]/pars$initS, pars$jCPm), na.rm=T)
+  jeL[1,] <- pmax(jL[1,] - jCP[1,]/pars$yCL, 0)
+  jNPQ[1,] <- pars$kNPQ
+  jCO2w[1,] <- jCO2[1]*H[1]/pars$initS - jCP[1,]
+  jSG[1,] <- pars$jSGm/10
+  rhoC[1,] <- jCP[1,]
+  jNw[1,] <- 0
+  jST[1,] <- pars$jST0
+  rCS[1,] <- pars$jST0 * pars$sigmaCS
+  cROS[1,] <- 1
+  dS.Sdt[1,] <- pars$jSGm
+  S[1,] <- pars$initS
+
+  # Run simulation by updating
+  # ==========================
+  dt <- time[2] - time[1]
+  for (t in 2:length(time)) {
+
+    # Photosynthesis fluxes
+    # =====================
+    # Light input flux
+    jL[t,] <- (1.256307 + 1.385969 * exp(-6.479055 * (sum(S[t-1,])/H[t-1]))) * env$L[t] * pars$astar
+    # CO2 input flux
+    rCS[t,] <- pars$sigmaCS * (jST[t-1,] + (1-pars$yC)*jSG[t-1,]/pars$yC)  # metabolic CO2 recycled from symbiont biomass turnover
+    # Production flux (photosynthetic carbon fixation)
+    jCP[t,] <- vsynth(jL[t,] * pars$yCL, (jCO2[t-1] + rCH[t-1])*H[t-1]/sum(S[t-1,]) + rCS[t,], pars$jCPm) / cROS[t-1,]
+    # Rejection flux: CO2 (wasted to the environment)
+    jCO2w[t,] <- pmax((jCO2[t-1] + rCH[t-1])*H[t-1]/sum(S[t-1,]) + rCS[t,] - jCP[t,], 0)
+    # Rejection flux: excess light energy not quenched by carbon fixation
+    jeL[t,] <- pmax(jL[t,] - jCP[t,]/pars$yCL, 0)
+    # Amount of excess light energy quenched by NPQ
+    jNPQ[t,] <- (pars$kNPQ^(-1)+jeL[t,]^(-1))^(-1/1)  # single substrate SU
+    # Scaled ROS production due to excess excitation energy (=not quenched by carbon fixation AND NPQ)
+    cROS[t,] <- 1 + (pmax(jeL[t,] - jNPQ[t,], 0) / pars$kROS)^pars$k
+    
+    # Symbiont biomass fluxes
+    # =======================
+    # Nitrogen input flux
+    rNS[t,] <- pars$jST0 * pars$nNS * pars$sigmaNS  # Recylced N from symbiont biomass turnover.
+    # Production flux (symbiont biomass formation)
+    jSG[t,] <- vsynth(pars$yC*jCP[t,], (rhoN[t-1]*H[t-1]/sum(S[t-1,]) + rNS[t,])/pars$nNS, pars$jSGm)
+    # Rejection flux: carbon (surplus carbon shared with the host)
+    rhoC[t,] <- pmax(jCP[t,] - jSG[t,]/pars$yC, 0)
+    # Total amount of carbon shared by all symbionts
+    rhoC.t <- sum(rhoC[t,]*S[t-1,])
+    # Rejection flux: nitrogen (surplus nitrogen wasted to the environment)
+    jNw[t,] <- max(H$rhoN[t-1]*H$H[t-1]/sum(S[t-1,]) + rNS[t,] - pars$nNS * jSG[t,], 0)
+    # Total amount of nitrogen wasted by all symbionts
+    jNw.t <- sum(jNw[t,]*S[t-1,])
+    # Symbiont biomass loss (turnover)
+    jST[t,] <- pars$jST0 * (1 + pars$b * (cROS[t,] - 1))
+    
+    # Host biomass fluxes
+    # ===================
+    # Food input flux (prey=both carbon and nitrogen)
+    jX[t] <- (pars$jXm * env$X[t] / (env$X[t] + pars$KX))  # Prey uptake from the environment
+    # Nitrogen input flux
+    jN[t] <- (pars$jNm * Ni[t] / (Ni[t] + pars$KN))  # N uptake as a function of nitrogen concentration in coral head (Ni) 
+    rNH[t] <- pars$jHT0 * pars$nNH * pars$sigmaNH  # Recycled N from host biomass turnover
+    # Production flux (host biomass formation)
+    jHG[t] <- synth(pars$yC*(rhoC.t/H[t-1] + jX[t]), (jN[t] + pars$nNX*jX[t] + rNH[t]) / pars$nNH, pars$jHGm)
+    # Rejection flux: nitrogen (surplus nitrogen shared with the symbiont)
+    rhoN[t] <- max(jN[t] + pars$nNX * jX[t] + rNH[t] - pars$nNH * jHG[t], 0)
+    # Rejection flux: carbon -- given back to symbiont as CO2 input to photosynthesis
+    jeC[t] <- max(jX[t] + rhoC.t/H[t-1] - jHG[t]/pars$yC, 0)
+    # carbon not used in host biomass is used to activate CCM's that deliver CO2 to photosynthesis
+    jCO2[t] <- pars$kCO2 * jeC[t] 
+    # Host biomass loss
+    jHT[t] <- pars$jHT0
+    # metabolic CO2 recycled from host biomass turnover
+    rCH[t] <- pars$sigmaCH * (jHT[t] + (1-pars$yC)*jHG[t]/pars$yC)
+
+    # Convert coral (H) biomass to biovolume for interactions with fish / nitrogen
+    VH[t] <- pars$kv * H[t]^pars$gamma
+    # Coral mortality rate (breakage as a function of colony volume VH)
+    M[t] <- pars$m * VH[t]^pars$mu
+
+    # Balance equations
+    # =================
+    # Host (H)
+    dH.Hdt[t] <- jHG[t] - pars$jHT0 - M[t] # Specific growth rates (Cmol/Cmol/d)
+    # Symbiont (S)
+    dS.Sdt[t,] <- jSG[t,] - jST[t,] - M[t] # Specific growth rates (Cmol/Cmol/d)
+    # Damselfish (P)
+    dP.Pdt[t] <- pars$rp * (pars$kp * VH[t] - pars$Bp * P[t] - pars$alpha.wp * W[t]) / (pars$kp * VH[t]) - pars$ap * env$U[t]
+    # Hawkfish (W)
+    dW.Wdt[t] <- pars$rw * (pars$kw * VH[t]^(2/3) - pars$Bw * W[t] - pars$alpha.pw * P[t]) / (pars$kw * VH[t]^(2/3)) - pars$aw * env$U[t]
+    # Internal DIN concentration (Ni)
+    dNi.dt[t] <- pars$D(env$N[t] - Ni[t]) + (pars$ep + pars$ew)/VH[t] + jNw.t/VH[t] - jN[t]*H[t-1]/VH[t]
+
+    # State variables
+    # ===============
+    H[t] <- H[t-1] + dH.Hdt[t] * H[t-1] * dt  # Biomass (Cmol)
+    S[t,] <- S[t-1,] + dS.Sdt[t,] * S[t-1,] * dt  # Biomass (Cmol)
+    P[t] <- P[t-1] + dP.Pdt[t] * P[t-1] * dt  # Biomass
+    W[t] <- W[t-1] + dW.Wdt[t] * W[t-1] * dt  # Biomass
+    Ni[t] <- Ni[t-1] + dNi.dt * dt # Ni concentration
+  }
+
+  # Return results
+  # ==============
+  out <- data.frame(
+    time, env$L, env$N, env$X, env$U, jX=jX, jN=jN, rNH=rNH, rhoN=rhoN, jeC=jeC, jCO2=jCO2, 
+    jHG=jHG, jHT=jHT, rCH=rCH, rNS=rNS, jL=jL, jCP=jCP, jeL=jeL, jNPQ=jNPQ, jCO2w=jCO2w, 
+    jSG=jSG, rhoC=rhoC, jNw=jNw, jST=jST, rCS=rCS, cROS=cROS, dH.Hdt=dH.Hdt, H=H, 
+    dS.Sdt=dS.Sdt, S=S, dP.Pdt=dP.Pdt, P=P, dW.Wdt=dW.Wdt, W=W, dNi.dt=dNi.dt, Ni=Ni)
+  return(out)
+}
