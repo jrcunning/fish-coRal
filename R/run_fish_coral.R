5766cfe1f03b7ea220441c1e166b36b961c18964
Ross Cunning <ross.cunning@gmail.com>
2018-03-28 13:55:09 -0400

    improve code formatting

--- R/run_fish_coral.R
+++ R/run_fish_coral.R
@@ -101,103 +101,96 @@ run_fish_coral <- function(time, env, pars) {
     # Symbiont biomass loss (turnover)
     jST[t,] <- pars$jST0 * (1 + pars$b * (cROS[t,] - 1))
     
-    NiMeanVect<-vector(length=20)
-    for(i in 1:length(NiMeanVect)){
-    if(i==1){NiMean<-Ni[t-1]}else{
-      if(Nit1>Ni[t-1])
-      {
-        if(i==2)
-        {
-          maxNi<-mean(c(Ni[t],Ni[t-1]))
-          minNi<-Ni[t-1]
-          
-        }else{
-          if(Ni[t]>Ni[t-1])
-          {
-            if(NiMean>(0.01*Ni[t-1]+0.99*Ni[t]))
-            {
-              maxNi<-NiMean
-            }else{
-              minNi<-NiMean
+    NiMeanVect <- vector(length = 20)
+    for (i in 1:length(NiMeanVect)) {
+      if (i == 1) {
+        NiMean <- Ni[t-1]
+      } else {
+        if (Nit1 > Ni[t-1]) {
+          if (i == 2) {
+            maxNi <- mean(c(Ni[t], Ni[t-1]))
+            minNi <- Ni[t-1]
+          } else {
+            if (Ni[t] > Ni[t-1]) {
+              if (NiMean > (0.01 * Ni[t-1] + 0.99 * Ni[t])) {
+                maxNi <- NiMean
+              } else {
+                minNi <- NiMean
+              }
+            } else {
+              maxNi <- NiMean
             }
-          }else{
-            maxNi<-NiMean
+          }
+        } else {
+          if (i == 2) {
+            maxNi <- Ni[t-1]
+            minNi <- max(0, mean(Ni[t], Ni[t-1]))
+          } else {
+            if (Ni[t] < Ni[t-1]) {
+              if (NiMean > (0.01 * Ni[t-1] + 0.99 * Ni[t])) {
+                maxNi <- NiMean
+              } else {
+                minNi <- NiMean
+              }
+            } else {
+              minNi <- NiMean
+            }
           }
         }
-        
-      }else{
-        if(i==2)
-        {
-          maxNi<-Ni[t-1]
-          minNi<-max(0,mean(Ni[t],Ni[t-1]))
-        }else{
-          if(Ni[t]<Ni[t-1])
-          {
-            if(NiMean>(0.01*Ni[t-1]+0.99*Ni[t]))
-            {
-              maxNi<-NiMean
-            }else{
-              minNi<-NiMean
-            }
-          }else{
-            minNi<-NiMean
-          }
-        }
+        NiMean <- mean(c(maxNi, minNi))
       }
-      NiMean<-mean(c(maxNi,minNi))
+      NiMeanVect[i] <- NiMean
+
+      # Host biomass fluxes
+      # ===================
+      # Food input flux (prey=both carbon and nitrogen)
+      jX[t] <- (pars$jXm * env$X[t] / (env$X[t] + pars$KX))  # Prey uptake from the environment
+      # Nitrogen input flux
+      #jN[t] <- (pars$jNm * Ni[t-1] / (Ni[t-1] + pars$KN))  # N uptake as a function of nitrogen concentration in coral head (Ni) 
+      jN[t] <- (pars$jNm * NiMean / (NiMean + pars$KN))  # N uptake as a function of nitrogen concentration in coral head (Ni) 
+      #rNH[t] <- pars$jHT0 * pars$nNH * pars$sigmaNH  # Recycled N from host biomass turnover
+      # Production flux (host biomass formation)
+      jHG[t] <- synth(pars$yC*(rhoC.t/H[t-1] + jX[t]), (jN[t] + pars$nNX*jX[t] + rNH) / pars$nNH, pars$jHGm)
+      # Rejection flux: nitrogen (surplus nitrogen shared with the symbiont)
+      rhoN[t] <- max(jN[t] + pars$nNX * jX[t] + rNH - pars$nNH * jHG[t], 0)
+      # Rejection flux: carbon -- given back to symbiont as CO2 input to photosynthesis
+      jeC[t] <- max(jX[t] + rhoC.t/H[t-1] - jHG[t]/pars$yC, 0)
+      # carbon not used in host biomass is used to activate CCM's that deliver CO2 to photosynthesis
+      jCO2[t] <- pars$kCO2 * jeC[t] 
+      # Host biomass loss
+      #jHT[t] <- pars$jHT0
+      # metabolic CO2 recycled from host biomass turnover
+      rCH[t] <- pars$sigmaCH * (pars$jHT0 + (1-pars$yC)*jHG[t]/pars$yC)
+      
+      # Convert coral (H) biomass to biovolume for interactions with fish / nitrogen
+      VH[t] <- pars$kv * H[t-1]^pars$gamma
+      VHi[t] <- VH[t] * pars$vi  # Internal volume of water
+      # Coral mortality rate (breakage as a function of colony volume VH)
+      M[t] <- pars$m * VH[t]^pars$mu
+      
+      # Balance equations
+      # =================
+      # Host (H)
+      dH.Hdt[t] <- jHG[t] - pars$jHT0 - M[t] # Specific growth rates (Cmol/Cmol/d)
+      # Symbiont (S)
+      dS.Sdt[t,] <- jSG[t,] - jST[t,] - M[t] # Specific growth rates (Cmol/Cmol/d)
+      # Damselfish (P)
+      dP.Pdt[t] <- pars$rp * (pars$kp * VH[t] - pars$Bp * P[t-1] - pars$alpha.wp * W[t-1]) / (pars$kp * VH[t]) - pars$ap * env$U[t]
+      # Hawkfish (W)
+      dW.Wdt[t] <- pars$rw * (pars$kw * VH[t]^(2/3) - pars$Bw * W[t-1] - pars$alpha.pw * P[t-1]) / (pars$kw * VH[t]^(2/3)) - pars$aw * env$U[t]
+      # Internal DIN concentration (Ni)
+      dNi.dt[t] <- pars$D * (env$N[t] - NiMean) + (pars$ep*P[t-1] + pars$ew*W[t-1] + sum(jNw[t,]*S[t-1,]) - jN[t]*H[t-1])/VHi[t]
+      
+      # State variables
+      # ===============
+      dt <- time[t] - time[t-1]
+      H[t] <- H[t-1] + dH.Hdt[t] * H[t-1] * dt  # Biomass (Cmol)
+      S[t,] <- S[t-1,] + dS.Sdt[t,] * S[t-1,] * dt  # Biomass (Cmol)
+      P[t] <- P[t-1] + dP.Pdt[t] * P[t-1] * dt  # Biomass
+      W[t] <- W[t-1] + dW.Wdt[t] * W[t-1] * dt  # Biomass
+      Ni[t] <- Ni[t-1] + dNi.dt[t] * dt  # Ni concentration
+      if (i == 1) Nit1 <- Ni[t]
     }
-    NiMeanVect[i]<-NiMean
-    #NiMeanVect[i]<-NiMean
-    # Host biomass fluxes
-    # ===================
-    # Food input flux (prey=both carbon and nitrogen)
-    jX[t] <- (pars$jXm * env$X[t] / (env$X[t] + pars$KX))  # Prey uptake from the environment
-    # Nitrogen input flux
-    #jN[t] <- (pars$jNm * Ni[t-1] / (Ni[t-1] + pars$KN))  # N uptake as a function of nitrogen concentration in coral head (Ni) 
-    jN[t] <- (pars$jNm * NiMean / (NiMean + pars$KN))  # N uptake as a function of nitrogen concentration in coral head (Ni) 
-    #rNH[t] <- pars$jHT0 * pars$nNH * pars$sigmaNH  # Recycled N from host biomass turnover
-    # Production flux (host biomass formation)
-    jHG[t] <- synth(pars$yC*(rhoC.t/H[t-1] + jX[t]), (jN[t] + pars$nNX*jX[t] + rNH) / pars$nNH, pars$jHGm)
-    # Rejection flux: nitrogen (surplus nitrogen shared with the symbiont)
-    rhoN[t] <- max(jN[t] + pars$nNX * jX[t] + rNH - pars$nNH * jHG[t], 0)
-    # Rejection flux: carbon -- given back to symbiont as CO2 input to photosynthesis
-    jeC[t] <- max(jX[t] + rhoC.t/H[t-1] - jHG[t]/pars$yC, 0)
-    # carbon not used in host biomass is used to activate CCM's that deliver CO2 to photosynthesis
-    jCO2[t] <- pars$kCO2 * jeC[t] 
-    # Host biomass loss
-    #jHT[t] <- pars$jHT0
-    # metabolic CO2 recycled from host biomass turnover
-    rCH[t] <- pars$sigmaCH * (pars$jHT0 + (1-pars$yC)*jHG[t]/pars$yC)
-
-    # Convert coral (H) biomass to biovolume for interactions with fish / nitrogen
-    VH[t] <- pars$kv * H[t-1]^pars$gamma
-    VHi[t] <- VH[t] * pars$vi  # Internal volume of water
-    # Coral mortality rate (breakage as a function of colony volume VH)
-    M[t] <- pars$m * VH[t]^pars$mu
-
-    # Balance equations
-    # =================
-    # Host (H)
-    dH.Hdt[t] <- jHG[t] - pars$jHT0 - M[t] # Specific growth rates (Cmol/Cmol/d)
-    # Symbiont (S)
-    dS.Sdt[t,] <- jSG[t,] - jST[t,] - M[t] # Specific growth rates (Cmol/Cmol/d)
-    # Damselfish (P)
-    dP.Pdt[t] <- pars$rp * (pars$kp * VH[t] - pars$Bp * P[t-1] - pars$alpha.wp * W[t-1]) / (pars$kp * VH[t]) - pars$ap * env$U[t]
-    # Hawkfish (W)
-    dW.Wdt[t] <- pars$rw * (pars$kw * VH[t]^(2/3) - pars$Bw * W[t-1] - pars$alpha.pw * P[t-1]) / (pars$kw * VH[t]^(2/3)) - pars$aw * env$U[t]
-    # Internal DIN concentration (Ni)
-    dNi.dt[t] <- pars$D * (env$N[t] - NiMean) + (pars$ep*P[t-1] + pars$ew*W[t-1] + sum(jNw[t,]*S[t-1,]) - jN[t]*H[t-1])/VHi[t]
-
-    # State variables
-    # ===============
-    dt <- time[t] - time[t-1]
-    H[t] <- H[t-1] + dH.Hdt[t] * H[t-1] * dt  # Biomass (Cmol)
-    S[t,] <- S[t-1,] + dS.Sdt[t,] * S[t-1,] * dt  # Biomass (Cmol)
-    P[t] <- P[t-1] + dP.Pdt[t] * P[t-1] * dt  # Biomass
-    W[t] <- W[t-1] + dW.Wdt[t] * W[t-1] * dt  # Biomass
-    Ni[t] <- Ni[t-1] + dNi.dt[t] * dt  # Ni concentration
-    if(i==1){Nit1<-Ni[t]}
-  }
   }
 
   # Return results
